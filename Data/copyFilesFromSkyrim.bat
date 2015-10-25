c635b90645050398bcf836d00e3102d111e55940
Jack Gilliam <Jack-Gilliam@hotmail.com>
2015-10-25 12:49:05 -0600

    Add a script to update files in the skyrim folder

---
+++ Data/copyFilesFromSkyrim.bat
@@ -0,0 +1,9 @@
+xcopy /s/y "C:\Program Files (x86)\Steam\SteamApps\common\skyrim\Data\Scripts\_mindbeingcharmed.pex" Scripts
+xcopy /s/y "C:\Program Files (x86)\Steam\SteamApps\common\skyrim\Data\Scripts\_mindconfig.pex" Scripts
+xcopy /s/y "C:\Program Files (x86)\Steam\SteamApps\common\skyrim\Data\Scripts\_mindeffectscript.pex" Scripts
+xcopy /s/y "C:\Program Files (x86)\Steam\SteamApps\common\skyrim\Data\Scripts\_mindscript.pex" Scripts
+xcopy /s/y "C:\Program Files (x86)\Steam\SteamApps\common\skyrim\Data\Scripts\Source\_mindbeingcharmed.psc" Scripts\Source
+xcopy /s/y "C:\Program Files (x86)\Steam\SteamApps\common\skyrim\Data\Scripts\Source\_mindconfig.psc" Scripts\Source
+xcopy /s/y "C:\Program Files (x86)\Steam\SteamApps\common\skyrim\Data\Scripts\Source\_mindeffectscript.psc" Scripts\Source
+xcopy /s/y "C:\Program Files (x86)\Steam\SteamApps\common\skyrim\Data\Scripts\Source\_mindscript.psc" Scripts\Source
+pause
---
+++ Data/copyFilesToSkyrim.bat
@@ -0,0 +1,2 @@
+xcopy /s/e Scripts "C:\Program Files (x86)\Steam\SteamApps\common\skyrim\Data\Scripts"
+pause
